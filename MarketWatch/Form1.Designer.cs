a5d6dbb8a0a0c365d8719e40c8e62ea2b886c848
ramtin <ramtinak@live.com>
2021-05-10 19:52:29 +0430

    Init

---
+++ MarketWatch/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace MarketWatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TimeLabel = new System.Windows.Forms.Label();
+            this.XCancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.TimeLabel.AutoSize = true;
+            this.TimeLabel.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TimeLabel.ForeColor = System.Drawing.Color.White;
+            this.TimeLabel.Location = new System.Drawing.Point(3, 7);
+            this.TimeLabel.Name = "label1";
+            this.TimeLabel.Size = new System.Drawing.Size(135, 33);
+            this.TimeLabel.TabIndex = 0;
+            this.TimeLabel.Text = "16:45:20";
+            // 
+            // button1
+            // 
+            this.XCancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.XCancelButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.XCancelButton.Font = new System.Drawing.Font("Consolas", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.XCancelButton.ForeColor = System.Drawing.Color.Silver;
+            this.XCancelButton.Location = new System.Drawing.Point(144, -5);
+            this.XCancelButton.Name = "button1";
+            this.XCancelButton.Size = new System.Drawing.Size(75, 56);
+            this.XCancelButton.TabIndex = 1;
+            this.XCancelButton.Text = "x";
+            this.XCancelButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.XCancelButton.UseVisualStyleBackColor = true;
+            this.XCancelButton.Click += new System.EventHandler(this.XCancelButtonClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(185, 46);
+            this.Controls.Add(this.XCancelButton);
+            this.Controls.Add(this.TimeLabel);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "MarketWatch";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TimeLabel;
+        private System.Windows.Forms.Button XCancelButton;
+    }
+}
+
---
+++ MarketWatch/Form1.cs
@@ -0,0 +1,126 @@
+﻿/*
+ * A tiny replacement app for Market watch [ Forex trading ]
+ * 
+ * Created by Ramtin Jokar [ Ramtinak@live.com ]
+ * 
+ * License: MIT
+ * 
+ */
+
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarketWatch
+{
+    public partial class Form1 : Form
+    {
+        #region Fields
+        readonly Timer Timer = new Timer();
+        readonly TimeZoneInfo Timezone = TimeZoneInfo.FindSystemTimeZoneById("Israel Standard Time");
+        bool IsBlack = true;
+        bool Clicked = false;
+        #endregion Fields
+
+        #region Constructor
+
+        public Form1()
+        {
+            InitializeComponent();
+            Load += FormLoad;
+            FormClosing += FormFormClosing;
+            Timer.Interval = 100;
+            Timer.Tick += TimerTick;
+            CheckForIllegalCrossThreadCalls = false;
+            MouseDown += FormMouseDown;
+            TimeLabel.MouseDown += FormMouseDown;
+           
+            StartPosition = FormStartPosition.Manual;
+
+            Location = new Point((Screen.PrimaryScreen.WorkingArea.Width / 2) - (Width / 2),
+                                   0);
+        }
+
+        #endregion Constructor
+
+        #region Event handlers
+
+        private void FormFormClosing(object sender, FormClosingEventArgs e)
+        {
+            Timer.Stop();
+        }
+
+        private void FormLoad(object sender, EventArgs e)
+        {
+            TopMost = true;
+            TopLevel = true;
+            Timer.Start();
+        }
+
+        private async void FormMouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+                // why this?>
+                // because Mouse click, double click won't work for me so
+                // I created a double click option by myself
+                if (Clicked)
+                {
+                    if (!IsBlack)
+                    {
+                        XCancelButton.BackColor = BackColor = Color.Black;
+                        TimeLabel.ForeColor = Color.White;
+                        XCancelButton.ForeColor = Color.Silver;
+                    }
+                    else
+                    {
+                        XCancelButton.BackColor = BackColor = Color.White;
+                        TimeLabel.ForeColor = Color.Black;
+                        XCancelButton.ForeColor = Color.Black;
+                    }
+                    IsBlack = !IsBlack;
+                }
+                Clicked = true;
+                await Task.Delay(100);
+                Clicked = false;
+            }
+        }
+
+        private void XCancelButtonClick(object sender, EventArgs e)
+        {
+            try
+            {
+                Application.ExitThread();
+                Application.Exit();
+            }
+            catch { }
+        }
+        private void TimerTick(object sender, EventArgs e)
+        {
+            try
+            {
+                var date = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, Timezone);
+                TimeLabel.Text = date.ToString("HH:mm:ss");
+            }
+            catch { }
+        }
+
+        #endregion Event handlers
+
+        #region DllImport
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        #endregion DllImport
+    }
+}
